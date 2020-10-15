4fd5ad96e205f11174ceb35aabe1430bf2a655cd
Carlos Esteban <carlosbcn169@gmail.com>
2020-10-15 18:12:32 +0200

    Add routes and modify dasboard view

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::get('/', 'BlogController@index')->name('blog');
+
+Route::get('/category/{id}', 'BlogController@getPostsCategory')->name('category');
+
+Route::get('/post/{id}', 'BlogController@showPost')->name('post');
+
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::resource('posts', 'PostController');
+
+Route::resource('categories', 'CategoryController');
